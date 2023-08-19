db728aaed4534e45f910f9920b01f54962d17df8
Dinesh Basnet <iamdineshbasnet@gmail.com>
2023-08-19 22:46:27 +0545

    listed blog posts successfully

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
 	const posts = await fetchBlogs();
 	return (
 		<main className="w-full h-full">
-			<div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-4 rounded-lg bg-slate-600 drop-shadow-xl">
+			<div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-slate-600 drop-shadow-xl">
 				<h1 className="text-slate-200 text-center text-2xl font-extrabold font-[ubuntu]">
 					Full Stack blog app with NEXTJS
 				</h1>
@@ -27,9 +27,31 @@ export default async function Home() {
 
 			<div className="w-full flex flex-col justify-center items-center">
 				{posts?.map((post: any) => (
-          <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-200 flex flex-col justify-center" key={post?.id}>
-            
-          </div>
+					<div
+						className="w-3/4 px-4 py-0 rounded-md mx-3 my-8 bg-slate-200 flex flex-col justify-center"
+						key={post?.id}>
+						<div className="flex items-center my-3">
+							<div className="mr-auto">
+								<div className="mr-auto font-semibold">
+									{post?.title}
+								</div>
+							</div>
+							<Link
+								href={`/blog/edit/${post?.id}`}
+								className="px-5 py-1 text-center text-sl bg-slate-900 rounded-md font-normal text-slate-200">
+								Edit 📝
+							</Link>
+						</div>
+
+						<div className="mr-auto my-1">
+							<blockquote className="font-bold text-slate-700">
+								{new Date(post?.date).toDateString()}
+							</blockquote>
+						</div>
+						<div className="mr-auto my-1">
+							<h2>{post?.description}</h2>
+						</div>
+					</div>
 				))}
 			</div>
 		</main>
